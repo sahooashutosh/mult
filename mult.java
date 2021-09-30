6add778ac4778df0d7d1e14c457c0563c3b1af5c
Ashutosh Prasad <45146282+ASHUTOSH57211@users.noreply.github.com>
2021-10-01 00:02:28 +0530

    Update mult.java
    a function to compare two character in string

--- mult.java
+++ mult.java
@@ -1,5 +1,24 @@
+iport java.util.*;
 class myClass{
 public static void main(){
+   Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+
+        String str = sc.nextLine();
+        int len = str.length();
+        int max = 0;
+        for(int i = 0; i<len ; i++){
+            int temp = 0;
+            for(int j = 0;j< len;j++){
+                if(str[i]==str[j]){
+                    temp=temp++;
+                }
+            }
+            if(temp > max){
+                max=temp;
+            }
+        }
+        System.out.println(max+"The hisgest number of letters in the word"+str);
 
 
 }
